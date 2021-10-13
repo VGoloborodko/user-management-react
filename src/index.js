fbf946b49bef4907db6ffdc8535cf0affef5c27e
VGoloborodko <goloborodko.vitali@gmail.com>
2021-10-13 16:21:44 +0300

    one

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import './css/index.css';
+import App from './App';
+
+class Index extends React.Component {
+  render() {
+    return (
+      <>
+        <App />
+      </>
+    );
+  }
+}
+
+const rootElement = document.querySelector('#root');
+ReactDOM.render(<Index />, rootElement);
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('#root')
+// );
